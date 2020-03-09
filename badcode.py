f3424a37be79182bd29bc97a420708f8aefa5980
ThomasMoorePizon <60993664+ThomasMoorePizon@users.noreply.github.com>
2020-03-09 13:35:44 -0400

    Add files via upload

---
+++ badcode.py
@@ -0,0 +1,17 @@
+def addtwo(x):
+    """Return the square of x.
+
+    >>> addtwo(2)
+    4
+    >>> addtwo(-2)
+    0
+    >>> addtwo(1)
+    3
+    """
+
+    return x + 3
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+
---
+++ badtestcase.py
@@ -0,0 +1,17 @@
+def square(x):
+    """Return the square of x.
+
+    >>> square(2)
+    4
+    >>> square(-2)
+    4
+    >>> square(1)
+    3
+    """
+
+    return x * x
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+
